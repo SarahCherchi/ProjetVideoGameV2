ae9d566dbcc7326a0022c3f7903441c12530ced9
SarahCherchi <sarah.cherchi@condorcet.be>
2023-07-07 19:23:01 +0200

    Connection DB + DAO

---
+++ ProjetVideoGameV2/Model/DAO/DAO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+
+namespace ProjetVideoGameV2.Model.DAO
+{
+    public abstract class DAO<T> 
+    {
+        protected string connectionString = null;
+        public DAO()
+        {
+            this.connectionString = ConfigurationManager.ConnectionStrings["VideoGamesDB"].ConnectionString;
+        }
+        public abstract bool Create(T obj);
+        public abstract bool Delete(T obj);
+        public abstract bool Update(T obj);
+        public abstract T Find(int id);
+        public abstract List<T> FindAll();
+
+    }
+}

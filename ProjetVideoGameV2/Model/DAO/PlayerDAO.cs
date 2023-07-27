338c7c6e30b421c649b44bac94b20aed1d79cd06
ryanvandamme <100210874+ryanvandamme@users.noreply.github.com>
2023-07-27 19:29:29 +0100

    mise en place choix nombre de semaine location

--- ProjetVideoGameV2/Model/DAO/PlayerDAO.cs
+++ ProjetVideoGameV2/Model/DAO/PlayerDAO.cs
@@ -176,7 +176,6 @@ namespace ProjetVideoGameV2.Model.Dao
                 }
             }
             return players;
-
         }
     }
 }
---
+++ ProjetVideoGameV2/View/AdminInputDialog/CreateLoanDialog.xaml.cs
@@ -0,0 +1,47 @@
+﻿using ProjetVideoGameV2.POCO;
+using System.Windows;
+
+namespace ProjetVideoGameV2.View.AdminInputDialog
+{
+    
+    public partial class CreateLoanDialog : Window
+    {
+        public int numberOfWeeks;
+        public Player player;
+        public VideoGames videoGames;
+        public CreateLoanDialog(Player player, VideoGames vg)
+        {
+            InitializeComponent();
+            this.player = player;
+            this.videoGames = vg;
+            lblGameName.Content = $"Number of weeks for {videoGames.Name}";
+        }
+
+        private void btnValider_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtNumberOfWeeks.Text, out int numberOfWeeks))
+            {
+                if(numberOfWeeks > 0)
+                {
+                    int totalCreditCost = numberOfWeeks * videoGames.CreditCost;
+                    if(totalCreditCost <= player.Credit) {
+                        this.numberOfWeeks = numberOfWeeks;
+                        DialogResult = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show($"You cannot book this game for {numberOfWeeks} weeks beacause you have {player.Credit} and the cost is {totalCreditCost} ", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("The number of weeks must be greater than 0 ", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid number of weeks. Please enter a number.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}

4eb045df281ca636a4c579ae8317df5301c3c56b
ryanvandamme <100210874+ryanvandamme@users.noreply.github.com>
2023-08-14 00:51:57 +0100

    Refactor Admin List of Loan Page

--- ProjetVideoGameV2/View/Admin_ListOfLoan.xaml.cs
+++ ProjetVideoGameV2/View/Admin_ListOfLoan.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using ProjetVideoGameV2.POCO;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ProjetVideoGameV2.View
 {
@@ -25,6 +14,11 @@ namespace ProjetVideoGameV2.View
         public Admin_ListOfLoan()
         {
             InitializeComponent();
+            initDgLoan();
+        }
+
+        private void initDgLoan()
+        {
             loans = Loan.findAllLoan();
             collectionView = CollectionViewSource.GetDefaultView(loans);
             dgLoans.ItemsSource = collectionView;
